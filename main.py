4576a6e44ca08c2843c7d7d099df55a9e4b0de83
Dean Martin García <dean.martin@gracia.lasalle.cat>
2025-12-04 18:13:57 +0100

    Dividimos el código para dependiendo el botón presionado cambie el código de ejecución y aplicamos la función clear( ) para cambiar la función aplicada anterior.

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
-#from microbit import *
+from microbit import *
 
-#while True:
-#    temperatura = input.temperature()
-#    led.plot_bar_graph(temperatura, 50)
+modo = 0
 
 x = 2
 y = 2
 
 while True:
-    led.plot(x, y)
-    basic.pause(50)
-    led.unplot(x, y)
 
-    ejeX = input.acceleration(Dimension.X)
-    ejeY = input.acceleration(Dimension.Y)
+    if input.button_is_pressed(Button.A):
+        modo = 1
+        basic.clear_screen()
+        
+    if input.button_is_pressed(Button.B):
+        modo = 2
+        basic.clear_screen()
+        
+    if modo == 1:
+        temperatura = input.temperature()
+        led.plot_bar_graph(temperatura, 50)
 
-    if ejeX < -150 and x > 0:
-        x = x - 1
+    if modo == 2:
+        led.plot(x, y)
+        basic.pause(50)
+        led.unplot(x, y)
 
-    if ejeX > 150 and x < 4:
-        x = x + 1
+        ejeX = input.acceleration(Dimension.X)
+        ejeY = input.acceleration(Dimension.Y)
 
-    if ejeY < -150 and y > 0:
-        y = y - 1
+        if ejeX < -150 and x > 0:
+            x = x - 1
 
-    if ejeY > 150 and y < 4:
-        y = y + 1
+        if ejeX > 150 and x < 4:
+            x = x + 1
+
+        if ejeY < -150 and y > 0:
+            y = y - 1
+
+        if ejeY > 150 and y < 4:
+            y = y + 1
--- main.ts
+++ main.ts
@@ -1,31 +1,47 @@
+let temperatura: number;
 let ejeX: number;
 let ejeY: number;
-// from microbit import *
-// while True:
-//     temperatura = input.temperature()
-//     led.plot_bar_graph(temperatura, 50)
+let modo = 0
 let x = 2
 let y = 2
 while (true) {
-    led.plot(x, y)
-    basic.pause(50)
-    led.unplot(x, y)
-    ejeX = input.acceleration(Dimension.X)
-    ejeY = input.acceleration(Dimension.Y)
-    if (ejeX < -150 && x > 0) {
-        x = x - 1
+    if (input.buttonIsPressed(Button.A)) {
+        modo = 1
+        basic.clearScreen()
     }
     
-    if (ejeX > 150 && x < 4) {
-        x = x + 1
+    if (input.buttonIsPressed(Button.B)) {
+        modo = 2
+        basic.clearScreen()
     }
     
-    if (ejeY < -150 && y > 0) {
-        y = y - 1
+    if (modo == 1) {
+        temperatura = input.temperature()
+        led.plotBarGraph(temperatura, 50)
     }
     
-    if (ejeY > 150 && y < 4) {
-        y = y + 1
+    if (modo == 2) {
+        led.plot(x, y)
+        basic.pause(50)
+        led.unplot(x, y)
+        ejeX = input.acceleration(Dimension.X)
+        ejeY = input.acceleration(Dimension.Y)
+        if (ejeX < -150 && x > 0) {
+            x = x - 1
+        }
+        
+        if (ejeX > 150 && x < 4) {
+            x = x + 1
+        }
+        
+        if (ejeY < -150 && y > 0) {
+            y = y - 1
+        }
+        
+        if (ejeY > 150 && y < 4) {
+            y = y + 1
+        }
+        
     }
     
 }
